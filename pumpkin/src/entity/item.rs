4230f1f5f702384b50f1f95155220ed17a765cca
deadfry42 <75206733+deadfry42@users.noreply.github.com>
2025-08-03 10:10:31 +0100

    Allow creative mode players to pick up items regardless of if they have (#1105)
    enough inventory room

--- pumpkin/src/entity/item.rs
+++ pumpkin/src/entity/item.rs
@@ -380,10 +380,11 @@ impl EntityBase for ItemEntity {
 
         if can_pickup
             && player.living_entity.health.load() > 0.0
-            && player
+            && (player
                 .inventory
                 .insert_stack_anywhere(&mut *self.item_stack.lock().await)
                 .await
+                || player.is_creative())
         {
             player
                 .client
