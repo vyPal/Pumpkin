ea2678a235344ad575d10cf8ed17173494c52714
CrabbyDisk <69647964+CrabbyDisk@users.noreply.github.com>
2025-08-15 08:02:05 +0000

    Fix broken nix flake, add rust-analyzer to devshell and move to fenix from rust-overlay (#1126)

--- flake.nix
+++ flake.nix
@@ -3,52 +3,59 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
+    fenix = {
+      url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    fenix,
+    ...
+  }:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = nixpkgs.lib.systems.flakeExposed;
       perSystem = {
         pkgs,
         system,
         ...
-      }:
-      let
-        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      }: let
+        toolchain = fenix.packages.${system}.stable.toolchain;
         manifest = (pkgs.lib.importTOML ./pumpkin/Cargo.toml).package;
-      in
-      {
+        workspace-manifest =
+          (pkgs.lib.importTOML ./Cargo.toml).workspace.package;
+      in {
         formatter = pkgs.nixfmt-rfc-style;
         _module.args.pkgs = import nixpkgs {
           inherit system;
-          overlays = [
-            (import inputs.rust-overlay)
-          ];
         };
 
-        devShells.default = pkgs.mkShell
-        {
-          nativeBuildInputs = with pkgs; [
-            (rustToolchain.override {
-                extensions = ["rust-src"];
-            })
-            pkg-config
-          ];
-        };
+        devShells.default =
+          pkgs.mkShell
+          {
+            nativeBuildInputs = with pkgs; [
+              toolchain
+              pkg-config
+            ];
+          };
 
-        packages.default = (pkgs.makeRustPlatform {rustc = rustToolchain; cargo = rustToolchain;}).buildRustPackage {
+        packages.default =
+          (pkgs.makeRustPlatform {
+            rustc = toolchain;
+            cargo = toolchain;
+          }).buildRustPackage {
             pname = manifest.name;
-            version = manifest.version;
+            version = workspace-manifest.version;
+
+            src = ./.;
 
             useFetchCargoVendor = true;
             cargoLock = {
-                lockFile = ./Cargo.lock;
+              lockFile = ./Cargo.lock;
             };
-        };
+          };
       };
     };
 }
