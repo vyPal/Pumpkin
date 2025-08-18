3b1fffcb6ea9a3a5c9b7692aa9426666863319c1
ht06 <hthug06@gmail.com>
2025-08-18 18:23:57 +0200

    feat: add french translation (#1135)

--- pumpkin-util/src/translation.rs
+++ pumpkin-util/src/translation.rs
@@ -17,6 +17,7 @@ use crate::text::{TextComponentBase, TextContent, style::Style};
 static VANILLA_EN_US_JSON: &str = include_str!("../../assets/en_us.json");
 static PUMPKIN_EN_US_JSON: &str = include_str!("../../assets/translations/en_us.json");
 static PUMPKIN_ES_ES_JSON: &str = include_str!("../../assets/translations/es_es.json");
+static PUMPKIN_FR_FR_JSON: &str = include_str!("../../assets/translations/fr_fr.json");
 static PUMPKIN_ZH_CN_JSON: &str = include_str!("../../assets/translations/zh_cn.json");
 static PUMPKIN_TR_TR_JSON: &str = include_str!("../../assets/translations/tr_tr.json");
 
@@ -190,6 +191,8 @@ pub static TRANSLATIONS: LazyLock<Mutex<[HashMap<String, String>; Locale::last()
             serde_json::from_str(PUMPKIN_EN_US_JSON).expect("Could not parse en_us.json.");
         let pumpkin_es_es: HashMap<String, String> =
             serde_json::from_str(PUMPKIN_ES_ES_JSON).expect("Could not parse es_es.json.");
+        let pumpkin_fr_fr: HashMap<String, String> =
+            serde_json::from_str(PUMPKIN_FR_FR_JSON).expect("Could not parse fr_fr.json.");
         let pumpkin_zh_cn: HashMap<String, String> =
             serde_json::from_str(PUMPKIN_ZH_CN_JSON).expect("Could not parse zh_cn.json.");
         let pumpkin_tr_tr: HashMap<String, String> =
@@ -203,6 +206,9 @@ pub static TRANSLATIONS: LazyLock<Mutex<[HashMap<String, String>; Locale::last()
         }
         for (key, value) in pumpkin_es_es {
             array[Locale::EsEs as usize].insert(format!("pumpkin:{key}"), value);
+        }
+        for (key, value) in pumpkin_fr_fr {
+            array[Locale::FrFr as usize].insert(format!("pumpkin:{key}"), value);
         }
         for (key, value) in pumpkin_zh_cn {
             array[Locale::ZhCn as usize].insert(format!("pumpkin:{key}"), value);
