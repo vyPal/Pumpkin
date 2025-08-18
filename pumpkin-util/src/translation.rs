398ba39233b89eb73b78791ff06acb38ade4973f
Crefax <48588995+Crefax@users.noreply.github.com>
2025-08-18 10:12:04 +0300

    feat(tr): Add Some Turkish translations (#1132)
    * feat(tr): Add Turkish translations
    
    * Fix formatting issue in translation.rs

--- pumpkin-util/src/translation.rs
+++ pumpkin-util/src/translation.rs
@@ -18,6 +18,7 @@ static VANILLA_EN_US_JSON: &str = include_str!("../../assets/en_us.json");
 static PUMPKIN_EN_US_JSON: &str = include_str!("../../assets/translations/en_us.json");
 static PUMPKIN_ES_ES_JSON: &str = include_str!("../../assets/translations/es_es.json");
 static PUMPKIN_ZH_CN_JSON: &str = include_str!("../../assets/translations/zh_cn.json");
+static PUMPKIN_TR_TR_JSON: &str = include_str!("../../assets/translations/tr_tr.json");
 
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash)]
 pub struct SubstitutionRange {
@@ -191,6 +192,8 @@ pub static TRANSLATIONS: LazyLock<Mutex<[HashMap<String, String>; Locale::last()
             serde_json::from_str(PUMPKIN_ES_ES_JSON).expect("Could not parse es_es.json.");
         let pumpkin_zh_cn: HashMap<String, String> =
             serde_json::from_str(PUMPKIN_ZH_CN_JSON).expect("Could not parse zh_cn.json.");
+        let pumpkin_tr_tr: HashMap<String, String> =
+            serde_json::from_str(PUMPKIN_TR_TR_JSON).expect("Could not parse tr_tr.json.");
 
         for (key, value) in vanilla_en_us {
             array[Locale::EnUs as usize].insert(format!("minecraft:{key}"), value);
@@ -203,6 +206,9 @@ pub static TRANSLATIONS: LazyLock<Mutex<[HashMap<String, String>; Locale::last()
         }
         for (key, value) in pumpkin_zh_cn {
             array[Locale::ZhCn as usize].insert(format!("pumpkin:{key}"), value);
+        }
+        for (key, value) in pumpkin_tr_tr {
+            array[Locale::TrTr as usize].insert(format!("pumpkin:{key}"), value);
         }
         Mutex::new(array)
     });
