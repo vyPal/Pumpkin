5474fcb9e5db0370f0c369e921fbe7b37fc18497
ChocoDev <100467857+chocodev11@users.noreply.github.com>
2026-03-06 23:28:22 +0700

    fix: use command sender dimension when tp (#1690)
    * fix: use target dismention
    
    * use from command sender
    
    * vanilla accurate
    
    ---------
    
    Co-authored-by: chocodev11 <chocodev11@users.noreply.github.com>

--- pumpkin/src/command/commands/teleport.rs
+++ pumpkin/src/command/commands/teleport.rs
@@ -52,6 +52,15 @@ fn yaw_pitch_facing_position(
     (yaw_degrees as f32, pitch_degrees as f32)
 }
 
+fn resolve_sender_world(
+    sender: &CommandSender,
+    server: &crate::server::Server,
+) -> std::sync::Arc<World> {
+    sender
+        .world()
+        .unwrap_or_else(|| server.worlds.load().first().unwrap().clone())
+}
+
 struct EntitiesToEntityExecutor;
 
 impl CommandExecutor for EntitiesToEntityExecutor {
@@ -65,7 +74,11 @@ impl CommandExecutor for EntitiesToEntityExecutor {
             let targets = EntitiesArgumentConsumer::find_arg(args, ARG_TARGETS)?;
 
             let destination = EntityArgumentConsumer::find_arg(args, ARG_DESTINATION)?;
-            let pos = destination.get_entity().pos.load();
+            let destination = destination.get_entity();
+            let pos = destination.pos.load();
+            let yaw = destination.yaw.load();
+            let pitch = destination.pitch.load();
+            let world = destination.world.load_full();
             if !World::is_valid(BlockPos(pos.floor_to_i32())) {
                 return Err(CommandError::CommandFailed(TextComponent::translate(
                     "commands.teleport.invalidPosition",
@@ -73,13 +86,9 @@ impl CommandExecutor for EntitiesToEntityExecutor {
                 )));
             }
             for target in targets {
-                let base_entity = target.get_entity();
-                let yaw = base_entity.yaw.load();
-                let pitch = base_entity.pitch.load();
-                let world = base_entity.world.load_full();
                 target
                     .clone()
-                    .teleport(pos, yaw.into(), pitch.into(), world)
+                    .teleport(pos, Some(yaw), Some(pitch), world.clone())
                     .await;
             }
 
@@ -109,14 +118,7 @@ impl CommandExecutor for EntitiesToPosFacingPosExecutor {
             }
             let facing_pos = Position3DArgumentConsumer::find_arg(args, ARG_FACING_LOCATION)?;
             let (yaw, pitch) = yaw_pitch_facing_position(&pos, &facing_pos);
-            //todo
-            let world = match sender {
-                CommandSender::Rcon(_) | CommandSender::Console | CommandSender::Dummy => {
-                    server.worlds.load().first().unwrap().clone()
-                }
-                CommandSender::Player(player) => player.world().clone(),
-                CommandSender::CommandBlock(_, w) => w.clone(),
-            };
+            let world = resolve_sender_world(sender, server);
 
             for target in targets {
                 target
@@ -135,8 +137,8 @@ struct EntitiesToPosFacingEntityExecutor;
 impl CommandExecutor for EntitiesToPosFacingEntityExecutor {
     fn execute<'a>(
         &'a self,
-        _sender: &'a CommandSender,
-        _server: &'a crate::server::Server,
+        sender: &'a CommandSender,
+        server: &'a crate::server::Server,
         args: &'a ConsumedArgs<'a>,
     ) -> CommandResult<'a> {
         Box::pin(async move {
@@ -152,16 +154,12 @@ impl CommandExecutor for EntitiesToPosFacingEntityExecutor {
             let facing_entity = EntityArgumentConsumer::find_arg(args, ARG_FACING_ENTITY)?;
             let (yaw, pitch) =
                 yaw_pitch_facing_position(&pos, &facing_entity.get_entity().pos.load());
+            let world = resolve_sender_world(sender, server);
 
             for target in targets {
                 target
                     .clone()
-                    .teleport(
-                        pos,
-                        Some(yaw),
-                        Some(pitch),
-                        facing_entity.get_entity().world.load_full(),
-                    )
+                    .teleport(pos, Some(yaw), Some(pitch), world.clone())
                     .await;
             }
 
@@ -175,7 +173,7 @@ struct EntitiesToPosWithRotationExecutor;
 impl CommandExecutor for EntitiesToPosWithRotationExecutor {
     fn execute<'a>(
         &'a self,
-        _sender: &'a CommandSender,
+        sender: &'a CommandSender,
         server: &'a crate::server::Server,
         args: &'a ConsumedArgs<'a>,
     ) -> CommandResult<'a> {
@@ -192,9 +190,8 @@ impl CommandExecutor for EntitiesToPosWithRotationExecutor {
             // Note: Rotation returns (yaw, is_yaw_relative, pitch, is_pitch_relative)
             // For teleport, we use absolute values only (ignore relative flags)
             let (yaw, _, pitch, _) = RotationArgumentConsumer::find_arg(args, ARG_ROTATION)?;
+            let world = resolve_sender_world(sender, server);
 
-            // todo command context
-            let world = server.worlds.load().first().unwrap().clone();
             for target in targets {
                 target
                     .clone()
@@ -226,14 +223,7 @@ impl CommandExecutor for EntitiesToPosExecutor {
                     [],
                 )));
             }
-            // todo command context
-            let world = match sender {
-                CommandSender::Rcon(_) | CommandSender::Console | CommandSender::Dummy => {
-                    server.worlds.load().first().unwrap().clone()
-                }
-                CommandSender::Player(player) => player.world().clone(),
-                CommandSender::CommandBlock(_, w) => w.clone(),
-            };
+            let world = resolve_sender_world(sender, server);
             for target in targets {
                 let yaw = target.get_entity().yaw.load();
                 let pitch = target.get_entity().pitch.load();
@@ -259,13 +249,14 @@ impl CommandExecutor for SelfToEntityExecutor {
     ) -> CommandResult<'a> {
         Box::pin(async move {
             let destination = EntityArgumentConsumer::find_arg(args, ARG_DESTINATION)?;
-            let pos = destination.get_entity().pos.load();
-            let world = destination.get_entity().world.load_full();
+            let destination = destination.get_entity();
+            let pos = destination.pos.load();
+            let yaw = destination.yaw.load();
+            let pitch = destination.pitch.load();
+            let world = destination.world.load_full();
 
             match sender {
                 CommandSender::Player(player) => {
-                    let yaw = player.living_entity.entity.yaw.load();
-                    let pitch = player.living_entity.entity.pitch.load();
                     if !World::is_valid(BlockPos(pos.floor_to_i32())) {
                         return Err(CommandError::CommandFailed(TextComponent::translate(
                             "commands.teleport.invalidPosition",
