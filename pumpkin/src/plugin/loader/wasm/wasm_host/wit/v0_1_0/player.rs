823914ec1c074f5c24772746fc45ace48f2b1e0f
Illyrius <28700752+illyrius666@users.noreply.github.com>
2026-03-10 12:47:46 +0100

    feat: wasm-api/player/get_name (#1840)
    * init
    
    Signed-off-by: illyrius666 <28700752+illyrius666@users.noreply.github.com>
    
    * fix fn order to match trait
    
    Signed-off-by: illyrius666 <28700752+illyrius666@users.noreply.github.com>
    
    * feat: add DowncastResourceExt impl for player
    
    ---------
    
    Signed-off-by: illyrius666 <28700752+illyrius666@users.noreply.github.com>
    Co-authored-by: Bjorn Beishline <75190918+BjornTheProgrammer@users.noreply.github.com>

--- pumpkin/src/plugin/loader/wasm/wasm_host/wit/v0_1_0/player.rs
+++ pumpkin/src/plugin/loader/wasm/wasm_host/wit/v0_1_0/player.rs
@@ -1,26 +1,57 @@
 use wasmtime::component::Resource;
 
 use crate::plugin::loader::wasm::wasm_host::{
+    DowncastResourceExt,
     state::{PlayerResource, PluginHostState},
     wit::v0_1_0::pumpkin::{self, plugin::player::Player},
 };
 
+impl DowncastResourceExt<PlayerResource> for Resource<Player> {
+    fn downcast_ref<'a>(&'a self, state: &'a mut PluginHostState) -> &'a PlayerResource {
+        state
+            .resource_table
+            .get_any_mut(self.rep())
+            .expect("invalid player resource handle")
+            .downcast_ref::<PlayerResource>()
+            .expect("resource type mismatch")
+    }
+
+    fn downcast_mut<'a>(&'a self, state: &'a mut PluginHostState) -> &'a mut PlayerResource {
+        state
+            .resource_table
+            .get_any_mut(self.rep())
+            .expect("invalid player resource handle")
+            .downcast_mut::<PlayerResource>()
+            .expect("resource type mismatch")
+    }
+
+    fn consume(self, state: &mut PluginHostState) -> PlayerResource {
+        state
+            .resource_table
+            .delete::<PlayerResource>(Resource::new_own(self.rep()))
+            .expect("invalid player resource handle")
+    }
+}
+
 impl pumpkin::plugin::player::Host for PluginHostState {}
 impl pumpkin::plugin::player::HostPlayer for PluginHostState {
+    async fn get_id(&mut self, player: Resource<Player>) -> String {
+        player
+            .downcast_ref(self)
+            .provider
+            .gameprofile
+            .id
+            .to_string()
+    }
+
+    async fn get_name(&mut self, player: Resource<Player>) -> String {
+        player.downcast_ref(self).provider.gameprofile.name.clone()
+    }
+
     async fn drop(&mut self, rep: Resource<Player>) -> wasmtime::Result<()> {
         let _ = self
             .resource_table
             .delete::<PlayerResource>(Resource::new_own(rep.rep()));
         Ok(())
-    }
-
-    async fn get_id(&mut self, player: Resource<Player>) -> String {
-        let resource = self
-            .resource_table
-            .get_any_mut(player.rep())
-            .expect("invalid player resource handle")
-            .downcast_ref::<PlayerResource>()
-            .expect("resource type mismatch");
-        resource.provider.gameprofile.id.to_string()
     }
 }
