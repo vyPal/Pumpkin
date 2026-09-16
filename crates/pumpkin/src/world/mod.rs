20d7ab0f440aaff2849667adda499e68caf1206c
BouncingElf10 <168445249+BouncingElf10@users.noreply.github.com>
2026-09-16 02:41:55 +0200

    feat(block): persist shulker box contents through break and place (#3451)
    * feat(block): added shulker box item component to store and apply items
    
    * feat(player): creative players make shulker boxes with items drop
    
    * fix: addressed fmt issue
    
    * fix: gate retrieving block entity

--- crates/pumpkin/src/world/mod.rs
+++ crates/pumpkin/src/world/mod.rs
@@ -5530,6 +5530,16 @@ impl World {
             return None;
         }
 
+        let mut flags = flags;
+        if flags.contains(BlockFlags::SKIP_DROPS)
+            && cause.is_some_and(|p| p.gamemode.load() == pumpkin_util::GameMode::Creative)
+            && self
+                .get_block_entity(position)
+                .is_some_and(|entity| entity.drops_for_creative_player())
+        {
+            flags.remove(BlockFlags::SKIP_DROPS);
+        }
+
         let mut event = BlockBreakEvent::new(
             cause.cloned(),
             broken_block,
@@ -5544,7 +5554,6 @@ impl World {
             return None;
         }
 
-        let mut flags = flags;
         if event.drop {
             flags.remove(BlockFlags::SKIP_DROPS);
         } else {
